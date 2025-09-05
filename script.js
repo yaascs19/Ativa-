179e9343dfc19aaa49c1b0fbbd161021414b7b44
yaascs19 <yasmincunegundes25@gmail.com>
2025-09-05 00:36:41 +0000

    Final Project

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector("#form");
+ 
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+   
+    if (form.checkValidity()) {
+      alert("O formulário foi enviado com sucesso!");
+      form.reset();
+    } else {
+      alert("Preencha todos os campos obrigatórios antes de enviar.");
+    }
+  });
+});
